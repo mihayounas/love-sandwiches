2d08d72b1c4a9b09200e56bdf271fdae687d4969
mihayounas <mihaelayounas55@gmail.com>
2022-05-15 11:49:54 +0000

    Clear some of the errors

--- run.py
+++ run.py
@@ -11,18 +11,19 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
-    print("Data should be six numbers,separated by commes.")
-    print("Example : 10,20,30,40,50,60\n")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here:")
-   
+    data_str = input("Enter your data here: ")
     sales_data = data_str.split(",")
     validate_data(sales_data)
+
 
 def validate_data(values):
     """
@@ -33,10 +34,10 @@ def validate_data(values):
     try:
         if len(values) != 6:
             raise ValueError(
-f"Exactly 6 values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e} , please try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
 
-    print(values)
+
 get_sales_data()
